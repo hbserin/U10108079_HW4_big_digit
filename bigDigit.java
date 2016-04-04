b96befa354bb9bc03c5f5fda24e22d1d3584eb30
hbserin <hbs_erin@yahoo.com.tw>
2016-04-04 20:17:12 +0800

    delete array list to use array

--- bigDigit.java
+++ bigDigit.java
@@ -4,35 +4,31 @@
  */
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 //main method
 public class bigDigit {
 	public static void main(String[] args) {
 
   	// Create a Scanner
-	  Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);
 
-	  //Create an array list
-	  ArrayList<Integer> list = new ArrayList<>();	  
-
-	  //Enter two numbers
-	  String firstNumber, secondNumber;
-	  System.out.print("Enter first number：");
-	  firstNumber = input.nextLine();
-	  ArrayList<Integer> arraylist1 = new ArrayList<>();
-	  for (int i = 0; i < firstNumber.length(); i++) {
-	    String singleNumber1 = firstNumber.substring(i, i+1);
-	    ary[i] = Integer.parseInt(singleNumber1);
-	  } 
-	  
-	  System.out.println("Enter second number：");
-	  secondNumber = input.nextLine();
-	  ArrayList<Integer> arraylist2 = new ArrayList<>();
-	  for (int i = 0; i < secondNumber.length(); i++) {
-	    String singleNumber2 = secondNumber.substring(i, i+1);
-	    ary[i] = Integer.parseInt(singleNumber2);
-	  } 	
+	//Enter two numbers
+	String firstNumber, secondNumber;
+	System.out.print("Enter first number：");
+	firstNumber = input.nextLine();
+	array1 = new int[firstNumber.length()];
+	for (int i = 0; i < firstNumber.length(); i++) {
+		String singleNumber1 = firstNumber.substring(i, i+1);
+		array1[i] = Integer.parseInt(singleNumber1);
+	} 
+	
+	System.out.println("Enter second number：");
+	secondNumber = input.nextLine();
+	array2 = new int[secondNumber.length()];
+	for (int i = 0; i < secondNumber.length(); i++) {
+		String singleNumber2 = secondNumber.substring(i, i+1);
+		array2[i] = Integer.parseInt(singleNumber2);
+	} 	
 
 	}
 }
